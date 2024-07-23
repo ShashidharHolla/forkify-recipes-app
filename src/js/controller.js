e65c5ad528aeb80de2bd798c881e05a585f20c3d
ShashidharHolla <shashidharholla6@gmail.com>
2024-07-23 22:38:53 +0530

    First commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -123,10 +123,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('New feature has been added!');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -135,6 +131,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHanderUpload(controlAddRecipe);
-  newFeature();
 };
 init();
